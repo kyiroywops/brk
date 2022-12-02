94bede9c5afadaa1d581c0edb54f3e8c00beb1b0
Kylian Leroy <aleexisazul@gmail.com>
2022-12-02 09:49:26 -0800

    cambios ye

--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Estudiante, Asignaturas, Enrolamiento
+
 
 # Create your views here.
+
+def estudiantes(request):
+    estudiantes = Estudiante.objects.all()
+    return render(request, 'estudiantes.html', {'estudiantes': estudiantes})
+
+def asignaturas(request):
+    asignaturas = Asignaturas.objects.all()
+    return render(request, 'asignaturas.html', {'asignaturas': asignaturas})
+
+def enrolamiento(request):
+    enrolamiento = Enrolamiento.objects.all()
+    return render(request, 'enrolamiento.html', {'enrolamiento': enrolamiento})
